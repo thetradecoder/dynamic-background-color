e75d078b1084e21af92d1d78335bf9a02e6d9d0b
unknown <thetradecoder@gmail.com>
2020-03-24 12:55:30 +0600

    Dynamic Background Color

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
 
-export default App;
+  componentDidMount(){
+    let container = document.createElement("div");
+container.style.height = "100vh";
+document.body.appendChild(container);
+container.addEventListener("mousemove", e => {
+  let x = e.clientX;
+  let y = e.clientY;
+  container.style.backgroundColor = "rgb(" + x + "," + y + "," + 100 + ")";
+});
+  }
+
+  render(){
+  return "";
+  }
+}
