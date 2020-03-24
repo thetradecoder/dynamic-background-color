c1f00d010ef94382c4d7f684dec8888b4ff1f56d
unknown <thetradecoder@gmail.com>
2020-03-24 12:53:23 +0600

    Updates

---
+++ precache-manifest.d85c70b436b0dcd483de90079be84975.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b6e32e65300fcad73d7db4065efc08",
+    "url": "/dynamic-background-color/index.html"
+  },
+  {
+    "revision": "2a0c962edeb83182633e",
+    "url": "/dynamic-background-color/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "df67163cefba156c7b60",
+    "url": "/dynamic-background-color/static/js/2.c73555f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dynamic-background-color/static/js/2.c73555f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a0c962edeb83182633e",
+    "url": "/dynamic-background-color/static/js/main.b80b16a8.chunk.js"
+  },
+  {
+    "revision": "99d1db513f4ee9b9d47f",
+    "url": "/dynamic-background-color/static/js/runtime-main.998217a9.js"
+  }
+]);
